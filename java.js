5de3a3ed067ecf527b69ed6d75c5815d78282959
maxim4o2 <123208866+maxim4o2@users.noreply.github.com>
2024-01-30 23:26:37 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+let slideshow=document.getElementById("slideimg");
+
+let imgarr=
+[
+"https://belk.scene7.com/is/image/Belk/wk04_022123_hp_fh5_carl1_m?$DWP_PHOTO$",
+"https://belk.scene7.com/is/image/Belk/wk04_022123_hp_fh5_carl3_m?$DWP_PHOTO$",
+"https://belk.scene7.com/is/image/Belk/wk04_022123_hp_fh5_carl2_m?$DWP_PHOTO$"
+];
+function slideshowFun(images){
+    let i=1;
+    setInterval(() => {
+      if(i==imgarr.length)i=0;
+      slideshow.setAttribute("src",imgarr[i])
+      i++;
+    }, 2000);
+  }
+  slideshowFun(slideshow);
+
+let imageIcon=document.getElementById('iconImage');
+
+imageIcon.addEventListener('click',()=>{
+  window.location.href='../index.html';
+})
